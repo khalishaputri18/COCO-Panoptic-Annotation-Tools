ef002bc25e120f9a1468210d42e21917c6b3c5dd
khalishaputri18 <66949610+khalishaputri18@users.noreply.github.com>
2023-12-21 04:03:28 +0700

    Update merge_label.py

--- merge_label.py
+++ merge_label.py
@@ -5,10 +5,10 @@ import os
 import os.path as osp
 import numpy as np
 
-ins_dir = 'annotations_ins1'
-sem_dir = 'annotations_sem1'
-pan_dir = 'annotations_pan'
-sem_dir2 = 'annotations_sem2'
+ins_dir = 'annotations_ins1_train2'     #png instance annotations input path (output from labelme2png_instance.py)
+sem_dir = 'annotations_sem1_train2'     #png semantic annotations input path (output from labelme2png_semantic.py)
+pan_dir = 'annotations_pan_train2'      #png panoptic annotation output path
+sem_dir2 = 'annotations_sem2_train2'    #png semantic annotations input path (output from labelme2png_semantic.py)
 
 ins_files = os.listdir(ins_dir)
 sem_files = os.listdir(sem_dir)
@@ -27,17 +27,12 @@ def main():
             #reverse the color, then convert the white color to black color from the semantic images
             if sem_img is not None:
                 reversed_sem_img= 255 - sem_img
-            
                 lower_white = np.array([200, 200, 200])  # lower threshold for white color
                 upper_white = np.array([255, 255, 255]) 
-                
                 white_mask = cv2.inRange(reversed_sem_img, lower_white, upper_white)
-
                 black_mask = cv2.bitwise_not(white_mask)
                 image_with_black_background = cv2.bitwise_and(reversed_sem_img, reversed_sem_img, mask=black_mask)
-
                 result = cv2.bitwise_and(reversed_sem_img, image_with_black_background)
-
                 cv2.imwrite(sem_img_path, result)
             else:
                 print('Error: Could not open or find the image.')
@@ -45,20 +40,16 @@ def main():
             
             if ins_img is not None and result is not None:
                 if ins_img.shape == result.shape:
-
                     panoptic_result = cv2.bitwise_or(ins_img, result)
                     panoptic_filename = f'{file_ins}'
                     panoptic_file_path = os.path.join(pan_dir, panoptic_filename)
 
                     cv2.imwrite(panoptic_file_path, panoptic_result)
-
                     print(f'Saved: {panoptic_file_path}')
-
                 else:
                     print(f'Error: Images {file_ins} and {corresponding_file} must have the same dimensions for bitwise operations.')
             else:
                 print(f'Error: Could not open or find one of the images: {ins_img_path}, {sem_img_path}')
-
         else:
             print(f'Warning: No corresponding file found for {file_ins} in the second folder.')
 
